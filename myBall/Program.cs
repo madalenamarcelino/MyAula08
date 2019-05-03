0e5e6cae1924f0b607bee6512a484c181f1a61c2
madalenamarcelino <madelynbennet@gmail.com>
2019-05-03 15:47:00 +0100

    class property

--- myBall/Program.cs
+++ myBall/Program.cs
@@ -2,11 +2,60 @@
 
 namespace myBall
 {
-    class Program
+    class Ball
     {
-        static void Main(string[] args)
+        // Instance variables
+        private Color color;
+        private float radius;
+        public int TimesThrown { get; private set; } = 0;
+
+        // Constructors
+        public Ball(Color color, float radius)
         {
-            Console.WriteLine("Hello World!");
+            this.color = color;
+            this.radius = radius;
+        }
+
+        // Operational methods this si a bit borin help
+        public void Pop()
+        {
+            radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (radius > 0)
+            {
+                TimesThrown++;
+            }
+        }
+
+    }
+    class Color
+    {
+        // Instance variables
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte Alpha { get; set; }
+
+        public byte Grayscale => (byte)((Red + Green + Blue) / 3);
+
+        // Constructors
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+
+        public Color(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = 255;
         }
     }
 }
